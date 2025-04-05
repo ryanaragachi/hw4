68a29458cc6c2c0d55fe36b7d516f26f1d04d88d
ryan aragachi <aragachi@usc.edu>
2025-04-05 03:15:10 +0000

    updates

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <algorithm>
 #endif
 
 #include "equal-paths.h"
@@ -9,10 +9,27 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool checkEqualPaths(Node* node, int depth, int& expectedLeafDepth) {
+    if (node == nullptr) return true;
+
+    if (node->left == nullptr && node->right == nullptr) {
+        if (expectedLeafDepth == -1) {
+            expectedLeafDepth = depth;
+        }
+        return depth == expectedLeafDepth;
+    }
+
+    return checkEqualPaths(node->left, depth + 1, expectedLeafDepth) &&
+           checkEqualPaths(node->right, depth + 1, expectedLeafDepth);
+}
+
+
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    int expectedLeafDepth = -1;
+    return checkEqualPaths(root, 0, expectedLeafDepth);
 
 }
 
